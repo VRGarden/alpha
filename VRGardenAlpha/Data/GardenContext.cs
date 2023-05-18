433301c1fdff95323f4a9aa3a924d20d816f2e3e
Civv <civv@vrc.garden>
2023-05-18 16:52:42 -0500

    Create project files

---
+++ VRGardenAlpha/Data/GardenContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VRGardenAlpha.Data
+{
+    public class GardenContext : DbContext
+    {
+        #pragma warning disable 8618
+        public GardenContext(DbContextOptions<GardenContext> options) : base(options) { }
+        #pragma warning restore 8618
+        
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ VRGardenAlpha/VRClassMap.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using VRGardenAlpha.Data;
+using VRGardenAlpha.Models;
+
+namespace VRGardenAlpha
+{
+    public class VRClassMap : Profile
+    {
+        public VRClassMap()
+        {
+            CreateMap<Post, PostModel>();   
+            CreateMap<Post, SearchablePost>(); 
+        }
+    }
+}
