71c2034f373290ace3fcdac90929a5062d5f490c
Civv <civv@vrc.garden>
2023-05-24 23:31:12 -0500

    Index tooling

--- VRGardenAlpha/Controllers/ModerationController.cs
+++ VRGardenAlpha/Controllers/ModerationController.cs
@@ -1,8 +1,13 @@
-﻿using Meilisearch;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Meilisearch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using VRGardenAlpha.Data;
 using VRGardenAlpha.Filters;
 using VRGardenAlpha.Models;
+using VRGardenAlpha.Models.Options;
 
 namespace VRGardenAlpha.Controllers
 {
@@ -10,14 +15,53 @@ namespace VRGardenAlpha.Controllers
     [Route("/moderation")]
     public class ModerationController : ControllerBase
     {
+        private readonly IMapper _mapper;
         private readonly GardenContext _ctx;
         private readonly MeilisearchClient _client;
+        private readonly GardenOptions _gardenOptions;
 
-        public ModerationController(MeilisearchClient client, GardenContext ctx)
+        public ModerationController(MeilisearchClient client, GardenContext ctx, IMapper mapper, IOptions<GardenOptions> gardenOptions)
         {
             _ctx = ctx;
             _client = client;
+            _mapper = mapper;
+            _gardenOptions = gardenOptions.Value;
         }
+
+        [HttpPost]
+        [RequiresMaster]
+        [Route("_search/settings/reindex")]
+        public async Task<IActionResult> ReindexAsync([FromQuery] string? remoteHost = null)
+        {
+            var index = _client.Index("vrcg-posts");
+            await index.DeleteAsync();
+
+            var posts = await _ctx.Posts
+                .ToListAsync();
+
+            var host = Request.Headers.Host;
+            string proto = Request.Headers["X-Forwarded-Proto"].FirstOrDefault() ??
+                (Request.IsHttps ? "https" : "http");
+
+            if (Request.Headers["X-Bot-Password"] == _gardenOptions.BotPassword)
+                host = remoteHost ?? host;
+
+            List<SearchablePost> sps = new();
+            foreach(var post in posts)
+            {
+                var sp = _mapper.Map<SearchablePost>(post);
+                sp.Thumbnail = proto + "://"
+                    + host + "/@storage/"
+                    + post.Id.ToString() + "_image"
+                    + (post.ImageContentType == "image/gif" ? ".gif" : ".jpg");
+
+                sps.Add(sp);
+            }
+
+            await index.AddDocumentsAsync(sps);
+            return NoContent();
+        }
+
 
         [HttpPost]
         [RequiresMaster]
@@ -53,7 +97,7 @@ namespace VRGardenAlpha.Controllers
                 "timestamp",
                 "timestampISO"
             });
-            
+
             return NoContent();
         }
 
