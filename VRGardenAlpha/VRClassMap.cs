24b77df7ae02f25d95c9256f4a14d1dbbe25b33c
Civv <civv@vrc.garden>
2023-05-30 15:09:01 -0500

    Update VRClassMap.cs

--- VRGardenAlpha/VRClassMap.cs
+++ VRGardenAlpha/VRClassMap.cs
@@ -14,6 +14,7 @@ namespace VRGardenAlpha
                     opt.MapFrom(src => src.Timestamp));
 
             CreateMap<Trade, TradeModel>();
+            CreateMap<TradeDetails, TradeDetailsModel>();
         }
     }
 }
