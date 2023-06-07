44027f1dcaca8e399d09a3aac51eb03bb017ac9d
Civv <civv@vrc.garden>
2023-06-06 21:09:32 -0500

    THIS SHOULD WORK

--- VRGardenAlpha/Services/Trading/UnityPackage.cs
+++ VRGardenAlpha/Services/Trading/UnityPackage.cs
@@ -1,6 +1,5 @@
 ﻿using ICSharpCode.SharpZipLib.GZip;
 using System.Formats.Tar;
-using System.Text;
 
 namespace VRGardenAlpha.Services.Trading
 {
@@ -10,66 +9,45 @@ namespace VRGardenAlpha.Services.Trading
         {
             try
             {
+                var final = Path.GetTempFileName();
                 using var stream = File.OpenRead(src);
                 using var gzip = new GZipInputStream(stream);
                 using var tar = new TarReader(gzip);
-
-                TarEntry? entry;
-                List<TarEntry> entries = new List<TarEntry>();
-                List<TarEntry> remove = new List<TarEntry>();
-                while((entry = await tar.GetNextEntryAsync()) != null)
-                    entries.Add(entry);
-
-                var final = Path.GetTempFileName();
-                foreach(var e in entries)
-                {
-                    if(e.Name.EndsWith("pathname"))
-                    {
-                        var path = Path.GetTempFileName();
-                        await e.ExtractToFileAsync(path, true);
-
-                        var data = File.ReadAllText(path);
-                        if(data.EndsWith("RuntimeCrashHandler.cs") 
-                            || data.EndsWith("VRCSDK.Analytics.dll") 
-                            || data.EndsWith("VRCSDK.Core.dll") 
-                            || data.EndsWith("VRCSDK.Editor.dll"))
-                        {
-                            var dir = Path.GetDirectoryName(e.Name);
-                            var asset = dir + "/asset";
-                            var meta = dir + "/meta";
-                            var pathname = dir + "/pathname";
-
-                            var assetEntry = entries.FirstOrDefault(x => x.Name == asset);
-                            var metaEntry = entries.FirstOrDefault(x => x.Name == meta);
-                            var pathnameEntry = entries.FirstOrDefault(x => x.Name == pathname);
-
-                            if (assetEntry != null)
-                                remove.Add(assetEntry);
-
-                            if (metaEntry != null)
-                                remove.Add(metaEntry);
-
-                            if (pathnameEntry != null)
-                                remove.Add(pathnameEntry);
-                        }
-                    }
-                }
-
                 using var outstream = File.OpenWrite(final);
                 using var gzipOut = new GZipOutputStream(outstream);
                 using var tarOut = new TarWriter(gzipOut);
-                foreach (var en in entries)
-                {
-                    if (remove.Contains(en))
-                        continue;
-                    else
-                        await tarOut.WriteEntryAsync(en);
-                }
 
-                // Ensure the streams are properly disposed
-                gzipOut.Finish();
-                gzipOut.Close();
-                outstream.Close();
+                TarEntry? entry;
+                List<string> entrySkip = new List<string>();
+                while ((entry = await tar.GetNextEntryAsync()) != null)
+                {
+                    if (entrySkip.Any(x => entry.Name.Contains(x)))
+                        continue;
+
+                    bool add = true;
+                    if (entry.Name.EndsWith("pathname"))
+                    {
+                        var path = Path.GetTempFileName();
+                        await entry.ExtractToFileAsync(path, true);
+
+                        var data = File.ReadAllText(path);
+                        if (data.EndsWith("RuntimeCrashHandler.cs")
+                            || data.EndsWith("VRCSDK.Analytics.dll")
+                            || data.EndsWith("VRCSDK.Core.dll")
+                            || data.EndsWith("VRCSDK.Editor.dll"))
+                        {
+                            var dir = Path.GetDirectoryName(entry.Name);
+                            entrySkip.Add(dir!);
+                            add = false;
+                        }
+
+                        File.Delete(path);
+
+                        if (add)
+                            await tarOut.WriteEntryAsync(entry);
+                    }
+                }
+                
                 tarOut.Dispose();
                 tar.Dispose();
 
